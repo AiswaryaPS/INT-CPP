b824fdf2883b843211ba598e8111ad334b7c6521
Aiswarya PS <69056143+AiswaryaPS@users.noreply.github.com>
2021-02-05 16:12:04 +0530

    Delete cal.cpp

--- calaadadadad/cal.cpp
+++
@@ -1,7 +0,0 @@
-#ifndef __CALC.H__
-#define __CALC.H__
-int addition(int num1,int num2);
-int subtraction(int num1,int num2);
-int multiplication(int num1,int num2);
-int division(int num1,int num2);
-#endif
